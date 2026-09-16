ce112331ddbbd16800114dd0d4097b43f4ac2bcb
nghia.pham <nghia.pham@techpro-sdx.com.vn>
2026-09-16 23:02:10 +0700

    docs: add database update scripts for media

---
+++ scripts/update-media-store.sql
@@ -0,0 +1,46 @@
+﻿START TRANSACTION;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260916153410_StandaloneMediaCatalog') THEN
+    DROP INDEX "IX_EntityImages_EntityType_EntityId_ImageType_IsUsed_SortOrder";
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260916153410_StandaloneMediaCatalog') THEN
+    ALTER TABLE "EntityImages" DROP COLUMN "EntityId";
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260916153410_StandaloneMediaCatalog') THEN
+    ALTER TABLE "EntityImages" DROP COLUMN "EntityType";
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260916153410_StandaloneMediaCatalog') THEN
+    ALTER TABLE "EntityImages" DROP COLUMN "ImageType";
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260916153410_StandaloneMediaCatalog') THEN
+    ALTER TABLE "EntityImages" DROP COLUMN "SortOrder";
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260916153410_StandaloneMediaCatalog') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20260916153410_StandaloneMediaCatalog', '10.0.11');
+    END IF;
+END $EF$;
+COMMIT;
+
---
+++ scripts/update-user-avatar.sql
@@ -0,0 +1,18 @@
+﻿START TRANSACTION;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260916153423_AddUserAvatarUrl') THEN
+    ALTER TABLE "AspNetUsers" ADD "AvatarUrl" text;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260916153423_AddUserAvatarUrl') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20260916153423_AddUserAvatarUrl', '10.0.11');
+    END IF;
+END $EF$;
+COMMIT;
+
